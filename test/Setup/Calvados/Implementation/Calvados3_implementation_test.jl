350d7c45bed9c36f5fc018e6f789f687e1dbd68b
ywitzky <134135819+ywitzky@users.noreply.github.com>
2025-07-14 10:44:09 +0200

    worked on Calvados3 test

--- test/Setup/Calvados/Implementation/Calvados3_implementation_test.jl
+++ test/Setup/Calvados/Implementation/Calvados3_implementation_test.jl
@@ -5,24 +5,35 @@ addprocs(5)
 @everywhere using HPSAnalysis
 @everywhere using PyCall
 
+#=
 BasePath = "$(SetupTestPath)/implementation_test/"
 if isdir(BasePath)
     rm(BasePath; force=true, recursive=true)
 end
 mkpath(BasePath)
+=#
 
-Proteins = ["tia1", "ubq2", "ubq3", "ubq4", "gal3"]
-DomainDict = Dict("tia1" => [(6, 82),(95, 172),(190, 275)], "ubq2" => [(11, 82),(87, 158)], "ubq3" => [(1, 72),(77, 148),(153, 224)], "ubq4" => [(1, 72),(77, 148),(153, 224),(229, 300)], "gal3" => [(117, 250)])
+@everywhere begin
+    BasePath = "/localscratch/test/Calvados3/SecondTry/"
 
-ProteinJSON = Dict("tia1" => "/localscratch/test/fold_tia1/fold_tia1_full_data_0.json", "ubq2" => "/localscratch/test/fold_ubq2/fold_ubq2_full_data_0.json", "ubq3" => "/localscratch/test/fold_ubq3/fold_ubq3_full_data_0.json", "ubq4" => "/localscratch/test/fold_ubq4/fold_ubq4_full_data_0.json", "gal3" => "/localscratch/test/fold_gal3/fold_gal3_full_data_0.json")
+    Proteins = ["tia1", "ubq2", "ubq3", "ubq4", "gal3"]
+    DomainDict = Dict("tia1" => [(6, 82),(95, 172),(190, 275)], "ubq2" => [(11, 82),(87, 158)], "ubq3" => [(1, 72),(77, 148),(153, 224)], "ubq4" => [(1, 72),(77, 148),(153, 224),(229, 300)], "gal3" => [(117, 250)])
 
-ProteinCif  = Dict("tia1" => "/localscratch/test/fold_tia1/fold_tia1_model_0.cif", "ubq2" => "/localscratch/test/fold_ubq2/fold_ubq2_model_0.cif", "ubq3" => "/localscratch/test/fold_ubq3/fold_ubq3_model_0.cif", "ubq4" => "/localscratch/test/fold_ubq4/fold_ubq4_model_0.cif", "gal3" => "/localscratch/test/fold_gal3/fold_gal3_model_0.cif")
+    PP = "/localscratch/test/"
+    PP ="/localscratch/test/Calvados3/AlphaFold3/"
 
-RgDict = Dict("tia1" => 2.645, "ubq2" => 2.076, "ubq3" => 2.649, "ubq4" => 3.230, "gal3" => 3.026)
+    ProteinJSON = Dict("tia1" => "$(PP)fold_tia1/fold_tia1_full_data_0.json", "ubq2" => "$(PP)fold_ubq2/fold_ubq2_full_data_0.json", "ubq3" => "$(PP)fold_ubq3/fold_ubq3_full_data_0.json", "ubq4" => "$(PP)fold_ubq4/fold_ubq4_full_data_0.json", "gal3" => "$(PP)fold_gal3/fold_gal3_full_data_0.json")
 
-pH = 7.0
-width_multiplier = 1.0
-Temperatures = 300.0
+    ProteinCif  = Dict("tia1" => "$(PP)fold_tia1/fold_tia1_model_0.cif", "ubq2" => "$(PP)fold_ubq2/fold_ubq2_model_0.cif", "ubq3" => "$(PP)fold_ubq3/fold_ubq3_model_0.cif", "ubq4" => "$(PP)fold_ubq4/fold_ubq4_model_0.cif", "gal3" => "$(PP)fold_gal3/fold_gal3_model_0.cif")
+
+    RgDict = Dict("tia1" => 2.645, "ubq2" => 2.076, "ubq3" => 2.649, "ubq4" => 3.230, "gal3" => 3.026)
+
+    pH = Dict("ubq4" => 8.0, "ubq3" => 8.0, "ubq2" => 8.0, "tia1"=>6.0, "gal3"=> 7.0)
+    width_multiplier = 1.0
+    Temperatures = Dict("ubq4"=>293.0, "ubq3" => 293.0, "ubq2" => 293.0, "tia1"=>293.15, "gal3"=> 303.0)
+    ionic = Dict("ubq4"=>0.33,"ubq3"=>0.33,"ubq2"=>0.33, "tia1"=>0.1, "gal3"=> 0.04)
+end
+
 
 @everywhere function Rg(protein, Path, Sim)
     HPSAnalysis.computeCOM!(Sim)
@@ -38,7 +49,7 @@ Temperatures = 300.0
     close(io)
 end
 
-@everywhere function run_sim_prot(protein, BasePath, DomainDict, ProteinJSON, ProteinCif, pH, width_multiplier, Temperatures)
+@everywhere function run_sim_prot(protein, BasePath, FoldedDomains, ProteinToJSON, ProteinToCif, pH, width_multiplier, Temperatures)
     PkgSourcePath = "/"*joinpath(split(pathof(HPSAnalysis),"/")[1:end-1])
     EnvironmentPath = HPSAnalysis.getPythonEnvironment(PkgSourcePath)
     ENV["PYCALL_JL_RUNTIME_PYTHON"] = "$(EnvironmentPath)/bin/python"
@@ -46,59 +57,50 @@ end
     sim = pyimport("Submit_HOOMD")
     func = pyimport("PythonFuncs")
 
-    RunsPerProtein = 1
-    FoldedDomains = DomainDict
-    ProteinToJSON = ProteinJSON
-    ProteinToCif = ProteinCif
+    temp = Temperatures[protein]   
+
+    pad = "001"
     mkpath(BasePath*"$(protein)")
-    for temp in Temperatures
-        pad = RunsPerProtein
-        mkpath(BasePath*"$(protein)/$(temp)K/")
-        mkpath(BasePath*"$(protein)/$(temp)K/RUN_$(pad)/")
-        mkpath(BasePath*"$(protein)/$(temp)K/RUN_$(pad)/Restart/")
-        Path = BasePath*"$(protein)/$(temp)K/RUN_$(pad)/"
-        
-        println(BasePath*"$(protein)/$(temp)K/RUN_$(pad)/")
-        Seq = HPSAnalysis.ProteinSequences.NameToSeq[protein]
-        NChains = 1
-        Sequences = [deepcopy(Seq) for _ in 1:NChains]
-        Proteins  = [deepcopy(protein) for _ in 1:NChains]
+    mkpath(BasePath*"$(protein)/$(temp)K/")
+    mkpath(BasePath*"$(protein)/$(temp)K/RUN_$(pad)/")
+    mkpath(BasePath*"$(protein)/$(temp)K/RUN_$(pad)/Restart/")
+    Path = BasePath*"$(protein)/$(temp)K/RUN_$(pad)/"
+    
+    println(BasePath*"$(protein)/$(temp)K/RUN_$(pad)/")
+    Seq = HPSAnalysis.ProteinSequences.NameToSeq[protein]
+    NChains = 1
+    Sequences = [deepcopy(Seq) for _ in 1:NChains]
+    Proteins  = [deepcopy(protein) for _ in 1:NChains]
 
-        Info = "SLAB Simulation script for $protein.\n\n"
-        BoxLengthShort = Float32(350.0)      
-        BoxLengthLong  = Float32(1500.)
-        BoxSize = [-BoxLengthShort/2., BoxLengthShort/2.,-BoxLengthLong/2., BoxLengthLong/2.,-BoxLengthShort/2., BoxLengthShort /2.]
+    Info = "SLAB Simulation script for $protein.\n\n"
+    BoxLengthShort = Float32(350.0)      
+    BoxLengthLong  = Float32(1500.)
+    BoxSize = [-BoxLengthShort/2., BoxLengthShort/2.,-BoxLengthLong/2., BoxLengthLong/2.,-BoxLengthShort/2., BoxLengthShort /2.]
 
-        SimulName = "$(protein)_$temp"
+    SimulName = "$(protein)_$temp"
 
-        (pos, Data) = HPSAnalysis.CreateStartConfiguration(SimulName,Path , Float32.([BoxLengthShort,BoxLengthShort*width_multiplier , BoxLengthShort]), Proteins, Sequences, Regenerate=true; Axis="y", SimulationType="Calvados3",ProteinToDomain=FoldedDomains,ProteinToCif=ProteinToCif)
+    (pos, Data) = HPSAnalysis.CreateStartConfiguration(SimulName,Path , Float32.([BoxLengthShort,BoxLengthShort*width_multiplier , BoxLengthShort]), Proteins, Sequences, Regenerate=true; Axis="y", SimulationType="Calvados3",ProteinToDomain=FoldedDomains,ProteinToCif=ProteinToCif)
 
-        ENM = HPSAnalysis.Setup.BuildENMModel(Data, FoldedDomains, Proteins, Sequences, ProteinToJSON)
+    ENM = HPSAnalysis.Setup.BuildENMModel(Data, FoldedDomains, Proteins, Sequences, ProteinToJSON)
 
-        HPSAnalysis.Setup.writeStartConfiguration(Path, "/$(protein)_slab","/$(SimulName)_Start_slab", Info, Sequences, BoxSize , 100_000_000, HOOMD=true ; SimulationType="Calvados3" , Temperature=temp,  InitStyle="Pos", Pos=pos , pH=pH,domain=FoldedDomains,Device="CPU",WriteOutFreq=100_000, ENM)
+    println(pos)
 
-        sim.run("$(Path)/")
+    HPSAnalysis.Setup.writeStartConfiguration(Path, "/$(protein)_slab","/$(SimulName)_Start_slab", Info, Sequences, BoxSize , 100_000_000, HOOMD=true ; SimulationType="Calvados3" , Temperature=temp,  InitStyle="Pos", Pos=pos , pH=pH[protein],domain=FoldedDomains,Device="CPU",WriteOutFreq=100_000, ENM)
 
-        _, _, _, IDToMass, _, _ = func.readDictionaries("$(Path)/HOOMD_Setup/Dictionaries.txt")
-        Masses = func.compute_Mass_List(Data.IDs, IDToMass)
-        Data.Masses = Masses
-        Data.ChainMasses = [sum(Masses)]
-        Rg(protein, Path, Data)
-    end
+    #=
+    sim.run("$(Path)/")
+
+    _, _, _, IDToMass, _, _ = func.readDictionaries("$(Path)/HOOMD_Setup/Dictionaries.txt")
+    Masses = func.compute_Mass_List(Data.IDs, IDToMass)
+    Data.Masses = Masses
+    Data.ChainMasses = [sum(Masses)]
+    Rg(protein, Path, Data)
+    =#
 end
 
-procs = workers()
-i = 1
-function nextproc()
-    global i
-    p = procs[i]
-    i = i % length(procs) + 1
-    return p
-end
-@sync begin
-    for protein in Proteins
-        @async remotecall_wait(run_sim_prot, nextproc(), protein, BasePath, DomainDict, ProteinJSON, ProteinCif, pH, width_multiplier, Temperatures)
-    end
+@everywhere function runStuff(protein)
+    run_sim_prot(protein, BasePath, DomainDict, ProteinJSON, ProteinCif, pH, width_multiplier, Temperatures)
 end
 
-#run_sim_prot("tia1", BasePath, DomainDict, ProteinJSON, ProteinCif, pH, width_multiplier, Temperatures)
+pmap(runStuff, Proteins)
+
--- test/Setup/GSD_test.jl
+++ test/Setup/GSD_test.jl
@@ -1,7 +1,11 @@
-filename_test="$(SetupTestPath)/GSD_write_test.gsd"
-filename="$(SetupTestPath)/GSD_write.gsd"
+filename_test="$(SetupTestPath)/GSD_write_manual.gsd"
+filename="$(SetupTestPath)/GSD_write_setup.gsd"
 
 using GSDFormat
+
+macro namedtest(name, test)
+    esc(:(@testset $name begin @test $test end))
+end
 
 @testset "writeGSDStartFile" begin
     #Not testing UseAngles=true, because it is an artefact of a previous version.
@@ -18,9 +22,14 @@ using GSDFormat
     coor =[0.5 6.24 0.0; 0.5 6.62 0.0; 0.5 7.0 0.0; 0.5 7.38 0.0]
     image=[0 0 0; 0 0 0; 0 0 0; 0 0 0]
 
-    for (num,atom) in enumerate(set)
-        AAToID[atom]=num
-    end
+
+    AAToID = Dict('M'=> Int32(1), 'N'=>Int32(3), 'A'=> Int32(2))
+#        AAToID = Dict('M'=> Int32(0), 'N'=>Int32(1), 'A'=> Int32(2))
+
+   #for (num,atom) in enumerate(set)
+   #     AAToID[atom]=num
+   # end
+
 
     snapshot=GSDFormat.Frame()
     snapshot.configuration.step=1
@@ -31,18 +40,19 @@ using GSDFormat
     snapshot.particles.position=coor
     IDToAA=Dict(value=>key for (key,value) in AAToID)
     snapshot.particles.types = ["M", "A", "N"]
-    snapshot.particles.typeid = [0, 2, 1, 0]
+    snapshot.particles.typeid = UInt32.([0, 2, 1, 0])
+
     snapshot.particles.image = image
     mass_charge=Vector{Float64}([1.0,2.0,3.0,1.0])
-    snapshot.particles.mass = mass_charge
-    snapshot.particles.charge = mass_charge
+    snapshot.particles.mass = Vector{Float32}([1.0,2.0,3.0,1.0])
+    snapshot.particles.charge = Vector{Float32}([1.0,2.0,3.0,1.0])
     snapshot.particles.diameter = [0.1, 0.3, 0.2, 0.1]
 
     snapshot.bonds.N=N-1
     snapshot.bonds.types = ["O-O"]
     snapshot.bonds.typeid = zeros(Int32, N-1)
     snapshot.bonds.group = [0 1; 1 2; 2 3]
-    
+
     DiMap=Dict()
     DiList=Matrix{Int}(undef,4,4)
 
@@ -50,9 +60,26 @@ using GSDFormat
     GSDFormat.append(file, snapshot)
     GSDFormat.close(file)
 
-    HPSAnalysis.Setup.writeGSDStartFile(filename,N,N-1,N-2,N-3,BoxSize,coor_notreshaped,AAToID,sequences,image_notreshaped,mass_charge,mass_charge,DiMap,DiList,AAToID,false, "test", [],[])
 
-    @test files_are_equal(filename_test,filename)
+    HPSAnalysis.Setup.writeGSDStartFile(filename,N,N-1,N-2,N-3,BoxSize,coor_notreshaped,AAToID,sequences,image_notreshaped,mass_charge,mass_charge,DiMap,DiList,AAToID,false, "Calvados2", [],[])
+
+
+    frame_setup = GSDFormat.open(filename, 'r')[1]
+    frame_manual = GSDFormat.open(filename_test, 'r')[1]
+
+    for container in ["particles","bonds","angles","dihedrals","impropers","constraints","pairs"]
+    #@testset "$container" begin 
+        snapshot_container = getproperty(frame_setup, Symbol(container))
+        test_container = getproperty(frame_manual, Symbol(container))
+
+            for name in GSDFormat.get_container_names(test_container)
+                @namedtest "$name"  getproperty(test_container,name) == getproperty(snapshot_container, name) 
+                #println("$name $(getproperty(test_container,name) == getproperty(snapshot_container, name))")
+            end
+        #end
+    end
+#    @test files_are_equal(filename_test,filename)
+
+    #rm(filename_test)
+    #rm(filename)
 end
-rm(filename_test)
-rm(filename)
